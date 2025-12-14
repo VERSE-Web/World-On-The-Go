c4c5b0113dea7c1d8f3609189fc36a5f45e97116
VERSE-Web <Verserxxx@proton.me>
2025-12-14 22:49:04 +0600

    Visited + Css integretion

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './country.css';
 
-const Country = () => {
+
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
+    const handleVisit = () => {
+        // console.log(`Visited ${country.name.common}`);
+        if(visited) {
+            setVisited(false);
+        } else {
+            setVisited(true);
+        }
+    }
+
     return (
-        <div>
-            <h3>Name</h3>
+        <div className='country'>
+        <h3>Name: {country.name.common}</h3>
+            <img
+                src={country.flags.flags.png}
+                alt={country.flags.alt} 
+            />
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'big country' : 'Small country'}</p>
+            <button onClick={handleVisit}>
+                {visited ? 'Visited' : 'Mark as visited'}
+            </button>
         </div>
     );
 };
