8287fe244f6ebc236535ca953dc3d9d251dde8d9
VERSE-Web <Verserxxx@proton.me>
2025-12-16 19:15:40 +0600

    The Last Stretch... The light at the end of the tunnel is here. we shall no longer wait men and women as this is the end for today.

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import './country.css';
 
 
-const Country = ({ country }) => {
+const Country = ({ country,handleVisitedCountries }) => {
     const [visited, setVisited] = useState(false)
-    const handleVisit = () => {
+    const handleVisited = () => {
         // console.log(`Visited ${country.name.common}`);
-        if(visited) {
-            setVisited(false);
-        } else {
-            setVisited(true);
-        }
+        // if(visited) {
+        //     setVisited(false);
+        // } else {
+        //     setVisited(true);
+        // }
+        setVisited(!visited);
+        handleVisitedCountries(country);
     }
-
     return (
         <div className='country'>
         <h3>Name: {country.name.common}</h3>
@@ -22,7 +23,7 @@ const Country = ({ country }) => {
             />
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? 'big country' : 'Small country'}</p>
-            <button onClick={handleVisit}>
+            <button onClick={handleVisited}>
                 {visited ? 'Visited' : 'Mark as visited'}
             </button>
         </div>
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,17 +1,33 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from '../Country/Country';
 import './countries.css';
 
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = (country) => {
+    console.log('handles visited countries', country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+    
+  }
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  console.log(countries);
   return (
     <div>
       <h1>In the countries: {countries.length}</h1>
+      <p>Total countries Visited: {visitedCountries.length}</p>
+      <ol>
+        {
+          visitedCountries.map(country => <li
+          key={country.cca3.cca3}
+          >{country.name.common}</li>)
+        }
+      </ol>
       <div id='countries'>
         {countries.map(country => <Country 
         key={country.cca3.cca3}
+        handleVisitedCountries={handleVisitedCountries}
         country={country}></Country>)}
       </div>
     </div>
